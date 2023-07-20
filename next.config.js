dc1a9a2ce43fe9c82d2d9b909c8bcba1855076ff
fisal <fisaljabbarin@gmail.com>
2023-07-20 02:16:56 +0200

    localhost changed again

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    APP_IP_ADDRESS: '172.32.84.138',
-  },
-}
+  reactStrictMode: true
+  }
 
 module.exports = nextConfig;
 
